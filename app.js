8e70945ee9babc2f8a75def265220a450b28cc62
annettesunday <annette.odhiambo@andela.com>
2018-10-08 18:08:46 +0300

    ch(config): setup mongodb and air data in

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var  MongoClient = require('mongodb');
+const bodyParser = require('body-parser');
+const db = require('./config/db')
+
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
-}).listen(3000);
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+// importing routes into app.js
 
 
-console.log('Waiting for request');
+MongoClient.connect(db.url, (err, database) => {
+    if (err)  return console.log(err);
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+        console.log('Hello world on' + ' ' + port);
+    })
+})
