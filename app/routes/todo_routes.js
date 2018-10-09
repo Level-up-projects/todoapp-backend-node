7ef9d7e0b09f035f795708dcfc4db586d2cc59ef
annettesunday <annette.odhiambo@andela.com>
2018-10-09 15:33:17 +0300

    [Feature ##161015226] Create delete endpoint

--- app/routes/todo_routes.js
+++ app/routes/todo_routes.js
@@ -1,3 +1,4 @@
+var ObjectID = require('mongodb').ObjectID
 module.exports = function(app, db) {
     app.post('/todo', (req, res) => {
         const todo = { text: req.body.body, title: req.body.title};
@@ -11,4 +12,17 @@ module.exports = function(app, db) {
 
         });
     });
+    app.delete('/todo/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id) }
+
+        db.collection('todo').remove(details, (err, item) => {
+            if(err) {
+                res.send({ 'error': 'An error occured'});
+            }
+            else{
+                res.send('Todo' + id + 'deleted')
+            }
+        })
+    })
 };
