d9f2bed0eab09b7223b50367b9ff7555e03013b4
annettesunday <annette.odhiambo@andela.com>
2018-10-09 15:17:24 +0300

    [Feature ##161015242] Create endpoint for getting todo

--- app/routes/todo_routes.js
+++ app/routes/todo_routes.js
@@ -1,14 +1,27 @@
+var ObjectID = require('mongodb').ObjectID
 module.exports = function(app, db) {
     app.post('/todo', (req, res) => {
-        const todo = { text: req.body.body, title: req.body.title};
+        const todo = { text: req.body.body, title: req.body.title };
         db.collection('todo').insert(todo, (err, result) => {
             if (err) {
-                res.send({ 'error': 'An error has occured'});
+                res.send({ 'error': 'An error has ocurred' });
             }
             else {
                 res.send(result.ops[0])
             }
 
+        });
+    });
+    app.get('/todo/:id', (req, res) => {
+        const id = req.params.id
+        const details = { '_id': new ObjectID(id) }
+        db.collection('todo').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured'});
+            }
+            else {
+                res.send(item)
+            }
         });
     });
 };
